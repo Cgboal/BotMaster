14e24fff2ce88f148db6ca60bc44f9bccb5c851d
cgboal <cgboal521@gmail.com>
2016-04-22 23:56:11 +0100

    Index page attempt 3

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-import BotApp
+from . import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(BotApp.urls)),
+    url(r'^$', views.index),
 )
