b9ae1dbf940c831677d97e4246c38036f27203eb
cgboal <cgboal521@gmail.com>
2016-04-23 18:37:54 +0100

    view attempt 4

--- BotApp/views.py
+++ BotApp/views.py
@@ -5,6 +5,10 @@ from .models import Bots
 # Create your views here.
 @csrf_exempt
 def index(request):
-    bot = Bots(request.POST['botId'], request.POST['botName'])
+    botId = request.POST.get('botId', False)
+    botName = request.POST.get('botName', False)
+    if not botId and not botName:
+        return render(request, 'index.html')
+    bot = Bots(request.POST.get('botId'), request.POST['botName'])
     bot.save()
-    return render(request, 'index.html')
+    return render(request, 'botAdded.html')
