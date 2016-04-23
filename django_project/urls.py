38146d9a738f75eec71aff8dbb58144dac395724
cgboal <cgboal521@gmail.com>
2016-04-23 18:56:09 +0100

    view attempt 5.3

--- django_project/urls.py
+++ django_project/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(urls)),
+    url(r'^test$', include(urls)),
 )
