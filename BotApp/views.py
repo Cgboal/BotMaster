0bd08c3bdc98a69a72844246973398824184ace0
cgboal <cgboal521@gmail.com>
2016-04-23 18:47:49 +0100

    view attempt 5.1

--- BotApp/views.py
+++ BotApp/views.py
@@ -7,8 +7,6 @@ from .models import Bots
 def index(request):
     botId = request.POST.get('botId', False)
     botName = request.POST.get('botName', False)
-    if not botId and not botName:
-        return render(request, 'index.html')
     bot = Bots(botId, botName)
     bot.save()
     return render(request, 'botAdded.html')
