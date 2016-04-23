eed4559d90a3b0df863d7c9c2e8f41ea9dfa319f
cgboal <cgboal521@gmail.com>
2016-04-23 19:44:28 +0100

    New bots can now be added to database

--- BotApp/models.py
+++ BotApp/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Bots(models.Model):
     botId = models.CharField(max_length=32)
     botName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.botName
