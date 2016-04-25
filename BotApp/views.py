413361d1e4f8be90299185f2aeb497bfbc4a6f6f
Cgboal <cgboal521@gmail.com>
2016-04-25 16:37:28 +0100

    Added time auth attempt 1

--- BotApp/views.py
+++ BotApp/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from .models import Bots
+import time, hashlib
 
 # Create your views here.
 @csrf_exempt
 def index(request):
     botId = request.POST.get('botId', False)
     botName = request.POST.get('botName', False)
-    if not botId and not botName:
-        return render(request, 'index.html')
+    auth = request.POST.get('auth', False)
+    if not botId or not botName or not auth:
+        if auth != genTimeSeed():
+            return render(request, 'index.html')
     bot = Bots(botId=botId, botName=botName)
     bot.save()
     return render(request, 'botAdded.html')
+
+def genTimeSeed():
+    seed = roundDown(int(time.time()), 5)
+    return hashlib.md5(seed).hexdigest()
+
+def roundDown(num, factor):
+    return num - (num%factor)
