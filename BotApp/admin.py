21e4d44911831bc9544771e530e7a159ae68387e
cgboal <cgboal521@gmail.com>
2016-04-26 17:56:54 +0100

    Added host to admin site

--- BotApp/admin.py
+++ BotApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Bot, Command
+from .models import Bot, Command, Host
 
 # Register your models here.
 admin.site.register(Bot)
 admin.site.register(Command)
+admin.site.register(Host)
