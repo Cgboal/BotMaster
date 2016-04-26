a3e167421f003be8ac25bb6a626b5c34419682f5
cgboal <cgboal521@gmail.com>
2016-04-26 15:50:20 +0100

    Fixed default value

--- BotApp/forms.py
+++ BotApp/forms.py
@@ -3,11 +3,11 @@ from .models import Command
 
 class cmdForm(forms.Form):
     command = forms.CharField(label="Command", max_length=100)
-    botId = forms.CharField(label="Bot ID", max_length=32, initial={'botId' : False})
+    botId = forms.CharField(label="Bot ID", max_length=32, initial='*')
 
     def process(self):
         cd = self.cleaned_data
-        if not cd['botId']:
+        if cd['botId'] == '*':
             cmd = Command(cmd=cd['command'], botId="*")
             cmd.save()
         else:
